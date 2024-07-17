7fd299a2f98487dd804524432b4dec306bed0638
laith abufadda <laith.abufadda@Eranovel.local>
2024-07-17 15:47:11 +0300

    services page

--- app/page.jsx
+++ app/page.jsx
@@ -3,50 +3,55 @@ import Socials from "@/components/Socials";
 import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button";
 import {FiDownload} from "react-icons/fi"
+import Services from "./services/page";
 
 
 export default function Home() {
   return (
-    <section className="h-full">
+    <>
+      <section className="h-full">
 
-      <div className="container h-full mx-auto">
+        <div className="container h-full mx-auto">
 
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-28">
+          <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-28">
 
-          <div className="text-center xl:text-left order-2 xl:order-none">
+            <div className="text-center xl:text-left order-2 xl:order-none">
 
-            <span className="text-xl">Software Developer</span>
-            <h1 className="h1 mb-6 mt-2">Hello I'm <br /> <span className="text-accent">Laith Abu Fadda</span></h1>
+              <span className="text-xl">Software Developer</span>
+              <h1 className="h1 mb-6 mt-2">Hello I'm <br /> <span className="text-accent">Laith Abu Fadda</span></h1>
 
-            <p className="max-w-[500px] mb-9 text-white/80">
-              I am a highly skilled Full Stack Web Developer proficient in various programming languages and new cutting-edge technologies.            
-            </p>
+              <p className="max-w-[500px] mb-9 text-white/80">
+                I am a highly skilled Full Stack Web Developer proficient in various programming languages and new cutting-edge technologies.            
+              </p>
 
-            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <div className="flex flex-col xl:flex-row items-center gap-8">
 
-                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-                  <span>Donwload CV</span>
-                  <FiDownload className="text-xl"/>
-                </Button>
+                  <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                    <span>Donwload CV</span>
+                    <FiDownload className="text-xl"/>
+                  </Button>
 
-                <div className="mb-8 xl:mb-0">
-                  <Socials containerStyle="flex gap-6" iconsStyle="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
-                </div>
+                  <div className="mb-8 xl:mb-0">
+                    <Socials containerStyle="flex gap-6" iconsStyle="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+                  </div>
 
+              </div>
+
+            </div>
+
+            <div className="order-1 xl:order-none mb-8 xl:mb-0">
+              <Photo/>
             </div>
 
           </div>
 
-          <div className="order-1 xl:order-none mb-8 xl:mb-0">
-            <Photo/>
-          </div>
-
         </div>
 
-      </div>
+        <Stats/>
 
-      <Stats/>
+      </section>
 
-    </section>
+      {/* <Services/> */}
+    </>
   );
 }
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,8 +1,80 @@
+"use client"
+
 import React from 'react'
+import Link from 'next/link'
+import {BsArrowDownRight} from "react-icons/bs"
+import { motion } from 'framer-motion'
+
+
+const services = [
+  {
+    num : "01" ,
+    href : "",
+    title : "Front End Developer" ,
+    desc : "I am a highly skilled Front-End Developer, adept at creating responsive and visually appealing web applications using modern frameworks and technologies."
+  },
+  {
+    num : "02" ,
+    href : "",
+    title : "Back End Developer" ,
+    desc : "I am a proficient Back-End Developer, experienced in building robust and scalable server-side applications using modern technologies and best practices."
+  },
+  {
+    num : "03" ,
+    href : "",
+    title : "Full Stack Developer" ,
+    desc : "I am a skilled Full Stack Developer, adept at creating comprehensive web applications by seamlessly integrating front-end and back-end technologies to deliver optimal performance and user experience."
+  },
+  {
+    num : "04" ,
+    href : "",
+    title : "Software Engineer:" ,
+    desc : "I am a versatile Software Engineer with a strong background in designing, developing, and maintaining complex software solutions across various domains, ensuring high-quality and efficient performance."
+  },
+]
+
+
 
 const Services = () => {
   return (
-    <div>Services</div>
+
+    <section id='services' className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0 mt-8'>
+
+      <h1 className='h1 text-center mb-20 text-accent'>Services .</h1>
+
+      <div className='container mx-auto'>
+
+        <motion.div className='grid grid-cols-1 md:grid-cols-2 gap-[70px]' initial={{opacity : 0}} animate={{opacity : 1 , transition : {delay : 2.4 , duration : 0.4 , ease : "easeIn"} }}>
+          
+          {services.map((service , index) => (
+            
+          <div className='flex-1 flex flex-col justify-center gap-6 group' key={index}>
+
+            <div className='w-full flex justify-between items-center'>
+
+                <div className='cursor-pointer text-5xl font-extrabold text-accent text-outline text-transparent group-hover:text-outline-hover'>{service.num}</div> 
+
+                <Link className='w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-300 flex justify-center items-center hover:-rotate-45' href={service.href}>
+                  <BsArrowDownRight className='text-primary text-3xl'/>
+                </Link>
+              
+            </div>
+            
+            <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500'>{service.title}</h2>
+            
+            <p className='text-white/60'>{service.desc}</p>
+
+            <div className='border-b border-white/20 w-full'></div>
+
+          </div>
+
+          ))}
+
+        </motion.div>
+      
+      </div>
+    
+    </section>
   )
 }
 
